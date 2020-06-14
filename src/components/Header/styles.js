32a1122233d90b548917738f73f73c94f46ec333
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2020-06-14 19:59:54 -0300

    Nubank app

---
+++ src/components/Header/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 30ox;
+`;
+
+export const Top = styled.View`
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 1px;
+`;
+
+export const Logo = styled.Image``;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  margin-left: 8px;
+`;
